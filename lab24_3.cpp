036b58d38ecd117783424c6077a4bf13c40a94af
PanTiwon <kkhowphuns@gmail.com>
2025-02-28 15:11:27 +0700

    Lab24_3 complete

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -3,6 +3,22 @@
 using namespace std;
 
 //Write function count() here.
+int count(int data[],int size){
+	int count = 0;
+	for(int i = 0; i < size; i++){
+		bool isUnique = true;
+		for(int j = 0; j < i; j++){
+			if(data[i] == data[j]){
+				isUnique = false;
+				break;
+			}
+		}
+		if(isUnique) {
+			count++;
+		}
+	}
+	return count;
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
